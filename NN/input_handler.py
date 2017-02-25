e808c6de9da3c1011fef3437b3095edb65e665b2
Dan Sarnelli <dsarnell@Dans-MacBook-Pro.local>
2017-02-25 15:34:54 -0500

    Added main files for testing the network.

---
+++ NN/input_handler.py
@@ -0,0 +1,9 @@
+def phrase_to_hash(phrase):
+	total = 0
+	char_count = {}
+	for i in range(32, 127):
+		char_count[i] = 0
+	for current_char in phrase:
+		total += ord(current_char)
+		char_count[ord(current_char)] += 1
+	return [0] * 100
---
+++ NN/main.py
@@ -0,0 +1,19 @@
+import input_handler
+import network
+import network_metadata
+
+network_structure = [100, 50, 1]
+net = network.Network(network_structure)
+net_tolerance = 0.5
+
+net.layers = network_metadata.layers
+net.biases = network_metadata.biases
+net.weights = network_metadata.weights
+
+def is_harassment(phrase):
+	current_hash = input_handler.phrase_to_hash(phrase)
+	network_output = net.eval(current_hash)
+	if network_output[0] > net_tolerance:
+		return 1
+	else:
+		return 0
